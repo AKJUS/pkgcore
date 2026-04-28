fb2c2c4cfe2da7c8abb0b5678a97794467bbf87f
Brian Harring <ferringb@gmail.com>
2026-04-28 14:10:25 +0200

    mark ToT as 0.12.34 for dealing with pip idiocy
    Signed-off-by: Brian Harring <ferringb@gmail.com>

--- src/pkgcore/__init__.py
+++ src/pkgcore/__init__.py
@@ -1,4 +1,4 @@
 __title__ = "pkgcore"
-__version__ = "0.12.33"
-__version_info__ = tuple(int(x) for x in __version__.split("."))
+__version__ = "0.12.34.dev0"
+__version_info__ = (0, 12, 34, "dev0")
 __python_mininum_version__ = (3, 12, 0)
